f0b003cf9875962ca4b79bfd1b628ab3d8948aa1
Pedro Rosa <pedro_sxbr@usp.br>
2023-04-21 12:01:38 +0200

    added Over blocks

--- blkOver.cpp
+++ blkOver.cpp
@@ -1,1 +1,35 @@
-// To be done
+#include "blkBlock.hpp"
+#include "blkOver.hpp"
+
+blk::expr blk::over(blk::expr op1, blk::expr op2){
+  return std::make_shared<blk::Over> (op1, op2);
+}
+
+blk::Over::Over(expr op1, expr op2) :
+  blk::Block(compute_width(op1, op2),
+             compute_height(op1, op2),
+             compute_ref_width(op1, op2),
+             compute_ref_height(op1,op2)),
+  op1(op1), op2(op2) {}
+
+void blk::Over::print_inbounds_line(std::ostream& os, int line) const{
+  int lpad;
+  int rpad;
+  int ref = (op1 -> get_height()) - (op1 -> get_ref_height());
+  if(line < ref){
+    lpad = std::max((op2 -> get_ref_width()) - (op1 -> get_ref_width()), 0);
+    rpad = std::max((op2 -> get_width() - (op2 -> get_ref_width())) -
+                   ((op1 -> get_width() - (op1 -> get_ref_width()))),0);
+    os << std::string(lpad, ' ');
+    op1 -> print_line(os, line);
+    os << std::string(rpad, ' ');
+  }
+  else{
+    lpad = std::max((op1 -> get_ref_width()) - (op2 -> get_ref_width()), 0);
+    rpad = std::max((op1 -> get_width() - (op1-> get_ref_width())) -
+                   ((op2 -> get_width() - (op2 -> get_ref_width()))),0);
+    os << std::string(lpad, ' ');
+    op2 -> print_line(os, (line - ref) - (op2 -> get_ref_height()));
+    os << std::string(rpad, ' ');    
+  }
+}
--- blkOver.hpp
+++ blkOver.hpp
@@ -1,4 +1,39 @@
 #pragma once
+#include "blkBlock.hpp"
 
-// To be done
+namespace blk {
+  expr over(expr op1, expr op2);
 
+  class Over : public Block {
+  private:
+    
+    expr op1;
+    expr op2;
+
+    // These functions make auxiliary size and ref computation for the
+    // resulting block, from the two operands. Use them as arguments
+    // to the conctruction of super class Block in the Beside
+    // constructor.
+    static int compute_width     (expr op1, expr op2){
+      return compute_ref_width(op1, op2) + std::max(op1 -> get_ref_width(), op2 -> get_ref_width());
+    }
+
+    static int compute_height    (expr op1, expr op2){
+      return (op1 -> get_height()) + (op2 -> get_height());
+    }
+
+    static int compute_ref_width (expr op1, expr op2){
+      return std::max(op1 -> get_ref_width(), op2 -> get_ref_width());
+    }
+
+    static int compute_ref_height(expr op1, expr op2){
+      return op1 -> get_ref_height();
+    }
+    
+  public:
+    
+    Over(expr op1, expr op2);
+    virtual ~Over() {}
+    virtual void print_inbounds_line(std::ostream& os, int line) const override;
+  };
+}
